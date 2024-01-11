7fca6e34fc6cb338d02c691a38bb5fc709776e28
Matteo <119304784+mfabbriconi@users.noreply.github.com>
2024-01-11 13:04:27 +0100

    Add files via upload

---
+++ getnextline/get_next_line_bonus.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfabbric <mfabbric@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 12:30:20 by mfabbric          #+#    #+#             */
+/*   Updated: 2023/01/30 15:19:17 by mfabbric         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <limits.h>
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*get_line(int fd, char *line);
+char	*ft_strchr(char *s, int c);
+char	*get_new_line(char *line);
+char	*get_next_line(int fd);
+char	*new_line(char *line);
+int		ft_strlen(char *s);
+
+#endif
